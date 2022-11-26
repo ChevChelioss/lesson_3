c85110af1ebdd0804f3f095100346fb4ca3a9fb2
chevchelios <keks1001@mail.ru>
2022-11-26 21:01:10 +0500

    update test from artur

--- test_example.py
+++ test_example.py
@@ -1,5 +1,3 @@
-
-
 def test_example_0():
     assert 10 > 1
 
@@ -7,4 +5,7 @@ def test_another_1():
     assert 0 != 1
 
 def test_another10():
-    assert 0 != 1
+    assert 0 != 14
+
+def test_another1111():
+    assert 0 != 111
